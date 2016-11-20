4e7a847abf9f817ad9760f1fc4872db46c852fb6
Hannes <hannes.betrieb@gmail.com>
2016-11-20 21:54:10 +0200

    Changed from dict to list of tuples

--- config_reader.py
+++ config_reader.py
@@ -3,4 +3,4 @@ from ConfigParser import ConfigParser
 def read_ip_user_names(fn_ini_file):
     config = ConfigParser()
     config.read(fn_ini_file)
-    return dict(config.items('IP_Device_Names'))
+    return list(tuple(config.items('IP_Device_Names')))
